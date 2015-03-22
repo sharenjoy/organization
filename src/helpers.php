ab6f82b68acd8dcda2a513b1421b6894d0f7c08d
Ronald Jian <ronald.jian@gmail.com>
2015-03-23 07:17:43 +0800

    add some helpers

--- src/helpers.php
+++ src/helpers.php
@@ -4,10 +4,31 @@ if ( ! function_exists('employee'))
 {
     /**
      * To output the employee object
+     */
+    function employee($id)
+    {
+        if (is_array($id))
+        {
+            return app()->make('Sharenjoy\Organization\Models\Employee')->whereIn('id', $id)->get();
+        }
+
+        return app()->make('Sharenjoy\Organization\Models\Employee')->find($id);
+    }
+}
+
+if ( ! function_exists('employeeByUserId'))
+{
+    /**
+     * To output the employee object
      * from the certain user
      */
-    function employee($userId = null)
+    function employeeByUserId($userId = null)
     {
+        if (is_array($userId))
+        {
+            return app()->make('Sharenjoy\Organization\Models\Employee')->whereIn('user_id', $userId)->get();
+        }
+
         if (is_null($userId))
         {
             $userId = session('user')['id'];
