222d1326de51aaa81aac30e2c8e7983b8b193ebe
Ronald <ronald.jian@gmail.com>
2015-05-02 17:45:20 +0800

    change config

--- config/organization.php
+++ config/organization.php
@@ -37,7 +37,7 @@ return [
     |
     */
 
-    'routeFromPackage' => false,
+    'routeFromPackage' => true,
 
     /*
     |--------------------------------------------------------------------------
